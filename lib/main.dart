cb995719acbf289902f86c2c94b86298811b19fa
Pro-ject-dev <138247328+Pro-ject-dev@users.noreply.github.com>
2025-08-21 15:27:31 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:amrita_gatepass/features/login/presentation/screens/login_screen.dart';
+import 'package:amrita_gatepass/features/Home/view/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home:HomeScreen(),
     );
   }
 }
